ffd5748c8db744e6c246fe9051142342ff19c02e
KallevX <kallevxofficial@gmail.com>
2025-07-15 17:10:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
-});
+  vite: {
+    server: {
+      allowedHosts: ['devserver-preview--kallevxaffiliate.netlify.app']
+    }
+  }
+})
